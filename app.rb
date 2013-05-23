44053267b4997fa5d3234c390a6189a72e5465b3
Joshua Szmajda <josh@optoro.com>
2013-05-23 11:48:17 -0400

    allowing cors

--- app.rb
+++ app.rb
@@ -12,11 +12,18 @@ require_relative './puzzler'
 
 cache = Dalli::Client.new
 
+def allow_origin!(headers)
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET'
+  headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, X-CSRF-Token'
+end
+
 get '/' do
   "Nothing to see here, move along folks."
 end
 
 get '/puzzle.json' do
+  allow_origin!(headers)
   i = cache.get('last_board_id').to_i
   board = Puzzler.new_puzzle(i + 1)
   cache.set('last_board_id', board.id)
@@ -25,6 +32,7 @@ get '/puzzle.json' do
 end
 
 post '/puzzle.json' do
+  allow_origin!(headers)
   board_id = params[:board_id]
   board_string = params[:solution]
   cached = cache.get("board-#{board_id}")
